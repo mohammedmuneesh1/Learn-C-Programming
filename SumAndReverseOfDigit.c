d1e7877547d2b51bcd0578e79cf29a11e2e9e3bf
Mohammed Muneesh E K <mohammedmuneesh1@gmail.com>
2024-02-29 12:20:55 +0530

    SumAndReverseOfDigit

--- SumAndReverseOfDigit.c
+++ SumAndReverseOfDigit.c
@@ -1,8 +1,14 @@
 #include<stdio.h>
 int main(){
-	int n;
-	printf("enter a number");
-	scanf("%d",&n);
-	printf("the number you entered is  %d",n);
-	return 0;
-	}
+    int number, sum=0, rev=0;
+    printf("Enter a number:");
+    scanf("%d",&number);
+    printf("Entered Number is : %d \n",number);
+    while(number>0){
+        sum=sum+number%10;
+        rev=(rev*10)+number%10;
+        number=number/10;
+    }
+    printf("Reverse of digit:%d \n",rev);
+    printf(" Sum of digit:%d",sum);
+}
